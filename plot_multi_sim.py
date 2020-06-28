0cd81c548fa7d1369e1c66807723f45148df9145
Callum-Leach <luxcaesium@protonmail.com>
2020-06-28 23:00:48 +0100

    added 4th plot

--- plot_multi_sim.py
+++ plot_multi_sim.py
@@ -15,20 +15,20 @@ filenames_2 = ["Processed/Simulation2/logs/p_0", "Processed/Simulation2/logs/p_1
 filenames_3 = ["Processed/Simulation3/logs/p_0", "Processed/Simulation3/logs/p_1", "Processed/Simulation3/logs/Ux_0", "Processed/Simulation3/logs/Uy_0", "Processed/Simulation3/logs/Uz_0", "Processed/Simulation3/logs/k_0", "Processed/Simulation3/logs/epsilon_0"]
 
 #4: Simulation1 initial tolerance
-#filenames_4 = ["Processed/Simulation4/logs/p_0", "Processed/Simulation4/logs/p_1", "Processed/Simulation4/logs/Ux_0", "Processed/Simulation4/logs/Uy_0", "Processed/Simulation4/logs/Uz_0", "Processed/Simulation4/logs/k_0", "Processed/Simulation4/logs/epsilon_0"]
+filenames_4 = ["Processed/Simulation4/logs/p_0", "Processed/Simulation4/logs/p_1", "Processed/Simulation4/logs/Ux_0", "Processed/Simulation4/logs/Uy_0", "Processed/Simulation4/logs/Uz_0", "Processed/Simulation4/logs/k_0", "Processed/Simulation4/logs/epsilon_0"]
 
 
 #read amnd generate dataframe from txt files:
 sim1_initial_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_1]
 sim2_initial_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_2]
 sim3_initial_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_3]
-#sim4_initial_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_4]
+sim4_initial_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_4]
 
 # Combine the dataframes
 sim1_initial_combined = pd.concat(sim1_initial_tolerance_df, ignore_index=False, axis=1)
 sim2_initial_combined = pd.concat(sim2_initial_tolerance_df, ignore_index=False, axis=1)
 sim3_initial_combined = pd.concat(sim3_initial_tolerance_df, ignore_index=False, axis=1)
-#sim4_initial_combined = pd.concat(sim4_initial_tolerance_df, ignore_index=False, axis=1)
+sim4_initial_combined = pd.concat(sim4_initial_tolerance_df, ignore_index=False, axis=1)
 
 # define a figure, with subplots as an array "ax" 
 fig, ax = plt.subplots(2,2)
@@ -38,7 +38,7 @@ fig, ax = plt.subplots(2,2)
 ax[0, 0].plot(sim1_initial_combined)
 ax[0, 1].plot(sim2_initial_combined)
 ax[1, 0].plot(sim3_initial_combined)
-#ax[1, 1].plot(sim4_initial_combined)
+ax[1, 1].plot(sim4_initial_combined)
 
 # plot 1 - (initial) residuals time series
 
@@ -125,7 +125,7 @@ ax[1, 0].set_xlim(1, len(sim3_initial_combined))
 ax[1, 0].legend(legend_1)
 
 
-'''
+
 # plot 4 - (initial) residuals time series
 
 # (optional) add horizontal lines to plot for idea of residuals relaxations.
@@ -153,7 +153,7 @@ ax[1, 1].set_xlim(1, len(sim4_initial_combined))
 
 # add legend to the plot
 ax[1, 1].legend(legend_1)
-'''
+
 
 
 # force plot to display in full-screen
