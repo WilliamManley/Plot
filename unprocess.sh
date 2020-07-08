72fdeb783fbff31463d130b99419544eb58b2226
Will Manley <wm395@bath.ac.uk>
2020-07-08 22:34:56 +0100

    unsure

--- unprocess.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash                                                                     
-for d in Processed/ ; do (cd "$d" && for d in ./*/ ; do (cd "$d" &&  chmod +x Allclean && ./Allclean && rm -R "$PWD"/logs && mv "$PWD" ../../Unprocessed); done ); done
