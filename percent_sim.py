bf69b4ed689475f2aef1e6f5b69d8cbf4a4b9ee7
Callum Leach <callumleach@callums-desktop.home>
2020-07-08 23:43:35 +0100

    Fixed calc issue

--- percent_sim.py
+++ percent_sim.py
@@ -9,23 +9,19 @@ import pandas as pd
 #1: initial tolerance files
 filenames_1 = ["Processed/Simulation1/logs/p_0", "Processed/Simulation2/logs/p_0", "Processed/Simulation3/logs/p_0", "Processed/Simulation4/logs/p_0", "Processed/Simulation5/logs/p_0"]
 #2: final tolerance files
-filenames_2 = ["Processed/Simulation1/logs/pFinalRes_0", "Processed/Simulation2/logs/pFinalRes_0", "Processed/Simulation3/logs/p_0", "Processed/Simulation4/logs/pFinalRes_0", "Processed/Simulation5/logs/pFinalRes_0"]
-#3: number of iterations files
-filenames_3 = ["Processed/Simulation1/logs/pIters_0", "Processed/Simulation2/logs/pIters_0", "Processed/Simulation3/logs/pIters_0", "Processed/Simulation4/logs/pIters_0", "Processed/Simulation5/logs/pIters_0"]
+filenames_2 = ["Processed/Simulation1/logs/pFinalRes_0", "Processed/Simulation2/logs/pFinalRes_0", "Processed/Simulation3/logs/pFinalRes_0", "Processed/Simulation4/logs/pFinalRes_0", "Processed/Simulation5/logs/pFinalRes_0"]
 
 #read and generate dataframe from txt files:
 initial_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_1]
 final_tolerance_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_2]
-num_iterations_df = [pd.read_csv(filename, names=[filename[5:]], sep="\t", engine='python') for filename in filenames_3]
 
 # Combine the dataframes
 initial_combined = pd.concat(initial_tolerance_df, ignore_index=False, axis=1)
 final_combined = pd.concat(final_tolerance_df, ignore_index=False, axis=1)
-num_iterations_combined = pd.concat(num_iterations_df, ignore_index=False, axis=1)
 
 #percentage change tolerance calculations
 #!!!Change range to fit number of simulations!!!
-percentage_combined_sim1 = pd.DataFrame(np.random.randint(1, 5, size=(min(len(initial_combined), len(final_combined)), 7)), columns=filenames_1)
+percentage_combined = pd.DataFrame(np.random.randint(1, 5, size=(min(len(initial_combined), len(final_combined)), 5)), columns=filenames_1)
 percentage_combined.index = np.arange(1,len(percentage_combined)+1)
 
 for i in range(0, len(initial_tolerance_df)):
@@ -34,30 +30,21 @@ for i in range(0, len(initial_tolerance_df)):
 #Generate the Plot
 plt.plot(percentage_combined)
 
-# plot 1 - (initial) residuals time series
+# plot 4 - % change in initial to final tolerance time series.
 
-# (optional) add horizontal lines to plot for idea of residuals relaxations.
-plt.axhline(y=1.0e-05, color="black", linestyle='--')
-plt.axhline(y=1.0e-04, color="black", linestyle='-')
-# instantaiate array for legend names
-legend_1 = []
+legend_1 = ['Sim1', 'Sim2', 'Sim3', 'Sim4', 'Sim5']
 # obtain legend names from file names
-for string in filenames_1:
-    # desired name in legend
-    new_string = string.replace("Processed/Simulation1/logs/", "")
-    #add (to end) of legend
-    legend_1.append(new_string)
+#for string in legend_1:
+#    legend_1.append(': p_0')
+
 # title of plot
-plt.title("Residual vs. Iteration")
+plt.title("Percentage change")
 # x axis label
 plt.xlabel("Iteration")
 # y axis label
-plt.ylabel("Residual")
-# log scale on y axis since skewed to small residuals
-plt.yscale("log")
+plt.ylabel("Percentage Change")
 # only plot to data range
-#!!! fix label issues with starting x axis!!!
-plt.xlim(1, len(initial_combined))
+plt.xlim(1, len(percentage_combined))
 
 # add legend to the plot
 plt.legend(legend_1)
